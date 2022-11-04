5d94211761c7b1d6f437bf116c2a2f692de35d00
Shiming Zhang <wzshiming@foxmail.com>
2022-11-04 18:04:57 +0800

    Update config

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p "${mountpoint}"
 mkdir -p /var/log/
 mkdir -p /etc/us3fs/
 
-cat << EOF >/etc/us3fs/us3fs.conf
+cat <<EOF >/etc/us3fs/us3fs.conf
 bucket: ${BUCKET}
 access_key: ${ACCESS_KEY}
 secret_key: ${SECRET_KEY}
@@ -17,32 +17,34 @@ EOF
 modprobe fuse
 /bin/us3fs -f --passwd=/etc/us3fs/us3fs.conf --keep_pagecache "${BUCKET}" "${mountpoint}" &
 
-for i in {1..10} ;do
-    if [ "$(df | grep -o ${mountpoint})" == "${mountpoint}" ]; then
-        echo "mount success"
-        break
-    fi
-    echo "wait the bucket mounting"
-    sleep 1
+for i in {1..10}; do
+  if [ "$(df | grep -o ${mountpoint})" == "${mountpoint}" ]; then
+    echo "mount success"
+    break
+  fi
+  echo "wait the bucket mounting"
+  sleep 1
 done
 
 if [ "$(df | grep -o ${mountpoint})" != "${mountpoint}" ]; then
-    echo "mount failed"
-    exit 1
+  echo "mount failed"
+  exit 1
 fi
 
 if [ ! -f "${checkfile}" ]; then
-    echo "${BUCKET}" > "${checkfile}"
+  echo "${BUCKET}" >"${checkfile}"
 else
-    if [ "$(cat "${checkfile}")" != "${BUCKET}" ]; then
-        echo "The bucket has been changed, please remove the old one and mount the new one."
-        echo "In ${checkfile} is ${BUCKET}, the old bucket is $(cat "${checkfile}")"
-        exit 1
-    fi
+  if [ "$(cat "${checkfile}")" != "${BUCKET}" ]; then
+    echo "The bucket has been changed, please remove the old one and mount the new one."
+    echo "In ${checkfile} is ${BUCKET}, the old bucket is $(cat "${checkfile}")"
+    exit 1
+  fi
 fi
 
 mkdir -p /etc/docker/registry/
-cat <<EOF > /etc/docker/registry/config.yml
+
+function registry_config() {
+  cat <<EOF
 version: 0.1
 log:
   accesslog:
@@ -58,14 +60,9 @@ storage:
     readonly:
       enabled: false
 http:
-  addr: :5000
+  addr: ${PORT:-0.0.0.0:5000}
   headers:
     X-Content-Type-Options: [nosniff]
-middleware:
-  storage:
-    - name: redirect
-      options:
-        baseurl: ${REDIRECT}
 health:
   storagedriver:
     enabled: false
@@ -73,16 +70,34 @@ health:
 validation:
   disabled: true
 
+EOF
+
+  if [[ "${REDIRECT}" != "" ]]; then
+    cat <<EOF
+middleware:
+  storage:
+    - name: redirect
+      options:
+        baseurl: ${REDIRECT}
+EOF
+  fi
+
+  if [[ "${REMOTE_URL}" != "" ]]; then
+    cat <<EOF
 proxy:
   remoteurl: ${REMOTE_URL}
-
 EOF
+  fi
+}
+
+registry_config >/etc/docker/registry/config.yml
+
 registry serve /etc/docker/registry/config.yml &
 
 echo "Registry started"
 while true; do
-    sleep 5
-    if [ ! -f "${checkfile}" ]; then
-        exit 1
-    fi
+  sleep 5
+  if [ ! -f "${checkfile}" ]; then
+    exit 1
+  fi
 done
